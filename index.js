8627120abdbdc3408b7294c3e51644a34484bc51
Luther Karper <luther.karper@live.com>
2020-03-05 14:57:24 -0500

    Globally applied middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();  // handles webserver functionality
+
+app.use(bodyParser.urlencoded({extended: true}));  // makes every route handler use bodyParser
 
 app.get('/', (req, res) => {  //req(uest) is information sent by user to server, res(ponse) is info sent from server to user
     res.send(`
@@ -15,25 +18,7 @@ app.get('/', (req, res) => {  //req(uest) is information sent by user to server,
     `);
 });
 
-// Middleware (always takes three arguements: req, res, next)
-const bodyParser = (req, res, next) => { 
-    if (req.method ==='POST') {
-        req.on('data', (data) => {  // works like an event listener
-            const parsed = data.toString('utf8').split('&'); //converts data from hex 
-            const formData = {};
-            for (let pair of parsed) {
-                const [key, value] = pair.split('=');
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();  // Called whenever we're ready to move on
-        });
-    } else {
-        next();
-    }
-};
-
-app.post('/', bodyParser, (req, res) => {  //handles POST method from the form
+app.post('/', (req, res) => {  //handles POST method from the form
     console.log(req.body);
     res.send("Account created");
 });
